97a1edf5d04ba837d5432f2320de87ea423b3d7f
Peter Torok <Peter Torok>
2022-07-22 11:05:56 +0200

    remove strict mode to resolve isLoading bug

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,7 @@ const client = new ApolloClient({
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <ApolloProvider client={client}>
-            <App />
-        </ApolloProvider>
-    </React.StrictMode>
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
 );
